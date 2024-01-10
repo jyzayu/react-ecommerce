a82a2044ee218f92d1406638b5763d2afeb30516
JY CHOI <moosou30@naver.com>
2024-01-10 14:57:19 +0900

    add component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ShoppingList from './shoppingList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App />
+    <ShoppingList />
     <App />
   </React.StrictMode>
 );
