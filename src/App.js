58c66b2ae0bd53ba856db7a6e9afd4db5b75dd89
JY CHOI <moosou30@naver.com>
2024-01-10 14:58:16 +0900

    sharing data between components from parent component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+//  sharing data between components from parent component
+function MyButton({ count, handleClick }) {
+  return (
+    <div>
+      <button onClick={handleClick}>Click {count} times!</button>
+    </div>
+  );
+}
 
 function App() {
+  // using hooks ,  function starting with use called  hooks
+  // You can only call Hooks at the top of your components
+  const [count, setCount] = useState(1);
+
+  function handleClick() {
+    setCount(count * 2);
+    console.log(count);
+  }
+
+  //  sharing data between components from parent component
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyButton count={count} handleClick={handleClick} />
+      <MyButton count={count} handleClick={handleClick} />
     </div>
   );
 }
